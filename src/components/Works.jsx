25076837aec039efff26598230327eaf1df2c3d7
unknown <akingungor@yahoo.com>
2023-05-01 14:12:47 -0400

    Adjust Work card Github button

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -32,7 +32,7 @@ const ProjectCard = ({
             className="w-full h-full object-cover rounded-2xl"
           />
 
-          <div className="absolute inset-0 flex justify-end m-3 card-img_hover">
+          <div className="absolute bottom-0 right-0 flex justify-end m-3 card-img_hover">
             <div
               onClick={() => window.open(source_code_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
@@ -41,7 +41,7 @@ const ProjectCard = ({
                 src={github}
                 alt="github"
                 className="w-1/2 h1/2 object-contain"
-              />
+              /> 
             </div>
             {/* Live Project Link */}
             {/* <div
