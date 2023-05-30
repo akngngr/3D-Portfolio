5be6e6ba693cc6471f6b04f816e582c40ff53e5b
unknown <akingungor@yahoo.com>
2023-05-29 20:50:02 -0400

    Fix a Work section bug in mobile

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -5,7 +5,7 @@ import { technologies } from "../constants/constants";
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10">
+    <div className="flex flex-row flex-wrap justify-center gap-10 h-max">
       {technologies.map((technology) => (
         <div className="w-28 h-28" key={technology.name}>
           <BallCanvas icon={technology.icon} />
--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -25,7 +25,7 @@ const ProjectCard = ({
           scale: 1,
           speed: 450,
         }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full my-5"
+        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full mt-10"
       >
         <div className="relative w-full h-[230px]"> 
           <img
