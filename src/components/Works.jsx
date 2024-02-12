bf735d9169883a5938a983ea54b771ef4950cd97
Akın Güngör <akingungor@yahoo.com>
2024-02-11 23:50:14 -0500

    Bug fixes for Mobile view within Works section

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -17,21 +17,15 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", 0.5 * index, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl w-[360px] mt-3"
-      >
+    <Tilt>
+      <motion.div variants={fadeIn("up", "spring", 0.5 * index, 0.75)}>
         <div
           options={{
             max: 45,
             scale: 1,
             speed: 450,
           }}
+          className="bg-tertiary p-5 rounded-2xl w-[360px] mt-3"
         >
           <div className="relative w-full h-[230px]">
             <img
@@ -68,14 +62,17 @@ const ProjectCard = ({
 
           <div className="mt-4 flex flex-wrap gap-2">
             {tags.map((tag) => (
-              <p key={`${name}-${tag.name}`} className={`text-[14px] ${tag.color}`}>
+              <p
+                key={`${name}-${tag.name}`}
+                className={`text-[14px] ${tag.color}`}
+              >
                 #{tag.name}
               </p>
             ))}
           </div>
         </div>
-      </Tilt>
-    </motion.div>
+      </motion.div>
+    </Tilt>
   );
 };
 
@@ -83,21 +80,21 @@ const Works = () => {
   return (
     <>
       <motion.div variants={textVariant()}>
-        <p className={`${styles.sectionSubText}`}>My work</p>
-        <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
+        <p className={styles.sectionSubText}>My work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
       </motion.div>
 
-      <div className='w-full flex'>
-      <motion.p
-        variants={fadeIn("up", "spring", 0.1, 1)}
-        className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
-        Following projects showcases my skills and experience through real-world
-        examples of my work. Each project is briefly described with links to
-        code repositories and live demos in it. It reflects my ability to solve
-        complex problems, work with different technologies, and manage projects
-        effectively.
-      </motion.p>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          Following projects showcases my skills and experience through
+          real-world examples of my work. Each project is briefly described with
+          links to code repositories and live demos in it. It reflects my
+          ability to solve complex problems, work with different technologies,
+          and manage projects effectively.
+        </motion.p>
       </div>
 
       <div className="my-5 flex flex-wrap justify-center gap-5">
