da49208a4cae1d395c2136bbd704d56b817ecd6d
Akın Güngör <akingungor@yahoo.com>
2024-02-11 22:24:11 -0500

    Bug fixes for Mobile view within Works section5

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -16,54 +16,56 @@ const ProjectCard = ({
   source_code_link,
 }) => {
   return (
-    <Tilt
-      options={{
-        max: 45,
-        scale: 1,
-        speed: 450,
-      }}
-      className="bg-tertiary p-5 rounded-2xl w-[360px] mt-3"
-    >
+    <Tilt className="bg-tertiary p-5 rounded-2xl w-[360px] mt-3">
       <motion.div variants={fadeIn("up", "spring", 0.5 * index, 0.75)}>
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt={name}
-            className="w-full h-full object-cover rounded-2xl"
-          />
+        <div
+          options={{
+            max: 45,
+            scale: 1,
+            speed: 450,
+          }}
+          
+        >
+          <div className="relative w-full h-[230px]">
+            <img
+              src={image}
+              alt={name}
+              className="w-full h-full object-cover rounded-2xl"
+            />
 
-          <div className="absolute bottom-0 right-0 flex justify-end m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:scale-125 ease-in duration-300"
-            >
-              <img
-                src={github}
-                alt="github"
-                className="w-1/2 h1/2 object-contain"
-              />
-            </div>
-            {/* Live Project Link */}
-            {/* <div
+            <div className="absolute bottom-0 right-0 flex justify-end m-3 card-img_hover">
+              <div
+                onClick={() => window.open(source_code_link, "_blank")}
+                className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer hover:scale-125 ease-in duration-300"
+              >
+                <img
+                  src={github}
+                  alt="github"
+                  className="w-1/2 h1/2 object-contain"
+                />
+              </div>
+              {/* Live Project Link */}
+              {/* <div
               onClick={() => window.open(project_link, "_blank")}
               className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
             >
               <img src="" alt="live" className="w-1/2 h1/2 object-contain" />
             </div> */}
+            </div>
           </div>
-        </div>
 
-        <div className="mt-5 ">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
+          <div className="mt-5 ">
+            <h3 className="text-white font-bold text-[24px]">{name}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
 
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p key={tag.name} className={`text-[14px] ${tag.color}`}>
-              #{tag.name}
-            </p>
-          ))}
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p key={tag.name} className={`text-[14px] ${tag.color}`}>
+                #{tag.name}
+              </p>
+            ))}
+          </div>
         </div>
       </motion.div>
     </Tilt>
@@ -78,14 +80,16 @@ const Works = () => {
         <h2 className={`${styles.sectionHeadText}`}>Projects.</h2>
       </motion.div>
 
-        <motion.p variants={fadeIn("", "", 0.1, 1)}
-        className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]">
-          Following projects showcases my skills and experience through
-          real-world examples of my work. Each project is briefly described with
-          links to code repositories and live demos in it. It reflects my
-          ability to solve complex problems, work with different technologies,
-          and manage projects effectively.
-        </motion.p>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Following projects showcases my skills and experience through real-world
+        examples of my work. Each project is briefly described with links to
+        code repositories and live demos in it. It reflects my ability to solve
+        complex problems, work with different technologies, and manage projects
+        effectively.
+      </motion.p>
 
       <div className="my-5 flex flex-wrap justify-center gap-5">
         {projects.map((project, index) => (
